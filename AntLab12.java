c1a0a3b2375783ddf971ef28b25e4ee0657c9253
ddean38 <55415298+ddean38@users.noreply.github.com>
2019-09-16 20:23:09 -0400

    Add files via upload

---
+++ AntLab12.java
@@ -0,0 +1,21 @@
+package edu.gatech.oad.antlab.pkg1;
+
+
+/**
+ * CS2335 Ant Lab
+ * <p>
+ * AntLab12.java helper class
+ */
+public class AntLab12 {
+
+
+    /**
+     * retrieves a pre-stored string message
+     *
+     * @return the string
+     */
+    public String getMessage() {
+        return " You";
+    }
+
+}
